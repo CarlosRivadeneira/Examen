36e453b51756059a8a5dfe9b2d061529c8d0044e
CarlosRivadeneira <carlosrvda@gmail.com>
2024-01-04 20:40:21 -0500

    Nuevos Cambios

--- Api.py
+++ Api.py
@@ -10,29 +10,6 @@ username = 'user'
 password = '123'
 cnxn = pyodbc.connect('DRIVER={ODBC Driver 17 for SQL Server};SERVER='+server+';DATABASE='+database+';UID='+username+';PWD='+ password)
 
-@app.route('/api/ciudad/<apellido>', methods=['GET'])
-def get_ciudad(apellido):
-    cursor = cnxn.cursor()
-    cursor.execute("SELECT nombre, apellido, usuario, ciudad FROM TuTabla WHERE apellido = ?", apellido)
-    row = cursor.fetchone()
-    
-    if row:
-        nombre = row[0]
-        apellido = row[1]
-        usuario = row[2]
-        ciudad = row[3]
-        response = requests.get(f"https://geocode.xyz/{ciudad}?json=1&auth=247521350360750201282x49376")
-        data = response.json()
-        if response.status_code == 200:
-            cursor.execute("INSERT INTO Ciudades2 (nombre, apellido, usuario, longitud, latitud) VALUES (?, ?, ?, ?, ?)",
-                           (nombre, apellido, usuario, data['longt'], data['latt']))
-            cnxn.commit()
-
-        return jsonify(data)
-    else:
-        return jsonify({"error": "No se encontró la ciudad en la base de datos"})
-
-@app.route('/api/ciudades2', methods=['GET'])
 @app.route('/api/ciudades2', methods=['GET'])
 def get_ciudades2():
     cursor = cnxn.cursor()
@@ -56,6 +33,24 @@ def get_ciudades2():
 
     ciudades = [{"nombre": row[0], "apellido": row[1], "usuario": row[2], "ciudad": row[3], "longitud": row[4], "latitud": row[5]} for row in rows]
     return jsonify(ciudades)
+
+@app.route('/api/catalogo_ciudades', methods=['GET'])
+def get_catalogo_ciudades():
+    cursor = cnxn.cursor()
+    cursor.execute("SELECT DISTINCT ciudad, longitud, latitud FROM Ciudades2")
+    rows = cursor.fetchall()
+
+    ciudades = [{"ciudad": row[0], "longitud": row[1], "latitud": row[2]} for row in rows]
+    return jsonify(ciudades)
+
+@app.route('/api/ciudades2/apellido/<apellido>', methods=['GET'])
+def get_clientes_por_apellido(apellido):
+    cursor = cnxn.cursor()
+    cursor.execute("SELECT nombre, usuario, ciudad, longitud, latitud FROM Ciudades2 WHERE apellido = ?", (apellido,))
+    rows = cursor.fetchall()
+
+    clientes = [{"nombre": row[0], "usuario": row[1], "ciudad": row[2], "longitud": row[3], "latitud": row[4]} for row in rows]
+    return jsonify(clientes)
 
 if __name__ == '__main__':
     app.run(port=3000)
